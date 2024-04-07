8764406e4cc9a0920b65bc2df933cc848f0930ca
jawwad-ali <alijawwad001@gmail.com>
2024-04-08 03:50:19 +0500

    add bootstrap card component

--- src/components/NewsFeeds.js
+++ src/components/NewsFeeds.js
@@ -20,18 +20,19 @@ const NewsFeeds = () => {
   }, []);
 
   return (
-    <div>
+    <div className="container row ">
       {data.map((item, idx) => (
-        <div key={idx}>
-          <h4>{item.title}</h4>
-          <p>{item.description}</p>
-          <img
-            src={item.urlToImage}
-            alt=""
-            style={{ height: "100px", width: "100px" }}
-          />
-
-          <hr />
+        <div className="col mb-5">
+          <div key={idx} className="card" style={{ width: "18rem" }}>
+            <img src={item.urlToImage} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{item.title}</h5>
+              <p className="card-text">{item.description}</p>
+              <a href={item.url} target="_blank" className="btn btn-primary">
+                Read News
+              </a>
+            </div>
+          </div>
         </div>
       ))}
     </div>
